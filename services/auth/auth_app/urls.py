86f5ac856cdeed5280eb77e327d628a86523fe25
David Garizado <dgarizad@c3r9s4.42malaga.com>
2024-11-21 17:23:56 +0100

    messing up all

--- services/auth/auth_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-
-urlpatterns = [
-    path('login', views.login_api_view, name='login'),
-	path('register', views.register_view, name='register'),
-]
--- services/websocket/websocket_app/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from websocket_app import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/room/(?P<room_name>\w+)/$', consumers.RoomConsumer.as_asgi()), 
-]
