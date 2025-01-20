b4b3ae6c104bc8db54900a7484b5f66d6a551eac
fer5899 <fdometallic@gmail.com>
2025-01-20 18:38:39 +0100

    Protect paddle update with redis transaction

--- services/game/game_app/consumers.py
+++ services/game/game_app/consumers.py
@@ -43,17 +43,9 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         self.redis = redis.from_url("redis://redis:6379")
 
-        if not await self.initialize_game_state():
-            return
+        self.determine_controllers()
 
-        await self.add_connection_to_group()
-
-        if self.game_group_name not in self.group_tasks:
-            self.group_tasks[self.game_group_name] = asyncio.create_task(
-                self.game_loop()
-            )
-
-        await self.save_and_broadcast_game_state()
+        self.update_game_state_task = asyncio.create_task(self.update_game_state())
 
     async def find_out_game(self):
         self.user_data = self.extract_user_data_from_jwt()
@@ -101,71 +93,59 @@ class GameConsumer(AsyncWebsocketConsumer):
                     break
         return user_data
 
-    async def initialize_game_state(self):
-        self.game_state = models.GameState.from_dict(self.initial_game_state)
-        self.game_state.ball.dx *= random.choice([-1, 1])
-        self.game_state.ball.dy *= random.choice([-1, 1])
-
-        if not (await self.determine_controllers()):
-            return False
-
-        return True
-
-    async def determine_controllers(self):
+    def determine_controllers(self):
         if (
             self.game.left_player_id == self.user_data["user_id"]
             and self.game.right_player_id == self.user_data["user_id"]
         ):
             self.left_paddle_controller = ["letters"]
             self.right_paddle_controller = ["arrows"]
-            return True
+
         if self.game.left_player_id == self.user_data["user_id"]:
             self.left_paddle_controller = ["letters", "arrows"]
             self.right_paddle_controller = []
             if self.game.right_player_id == 0:
                 self.right_paddle_controller.append("computer")
-            return True
+
         if self.game.right_player_id == self.user_data["user_id"]:
             self.left_paddle_controller = []
             self.right_paddle_controller = ["letters", "arrows"]
             if self.game.left_player_id == 0:
                 self.left_paddle_controller.append("computer")
-            return True
-
-    async def add_connection_to_group(self):
-        self.game_group_name = f"game_{self.game.id}"
-        await self.channel_layer.group_add(self.game_group_name, self.channel_name)
 
     async def send_error_and_close(self, error_message):
         await self.send(text_data=json.dumps({"error": error_message}))
         await self.close()
 
-    async def save_and_broadcast_game_state(self):
-        await self.redis.set(
+    async def load_game_state(self, redis_client):
+        game_state_data = await redis_client.get(f"game:{self.game.id}")
+        if game_state_data:
+            game_state_serializer = serializers.GameStateSerializer(
+                data=json.loads(game_state_data)
+            )
+            if game_state_serializer.is_valid():
+                self.game_state = models.GameState.from_dict(
+                    game_state_serializer.validated_data
+                )
+            else:
+                raise Exception(f"Invalid game state: {game_state_serializer.errors}")
+        else:
+            raise Exception("Game state not found")
+
+    async def save_game_state(self, redis_client):
+        await redis_client.set(
             f"game:{self.game.id}", json.dumps(self.game_state.to_dict())
         )
-        await self.channel_layer.group_send(
-            self.game_group_name,
-            {"type": "game_state_update", "game_state": self.game_state.to_dict()},
-        )
 
-    async def game_state_update(self, event):
-        serializer = serializers.GameStateSerializer(data=event["game_state"])
-        if not serializer.is_valid():
-            print(f"Errors: {serializer.errors}")
-            return
-        self.game_state = models.GameState.from_dict(serializer.validated_data)
+    async def send_game_state(self):
         await self.send(
             text_data=json.dumps(
-                {"type": "game_state_update", "game_state": serializer.validated_data}
+                {"type": "game_state_update", "game_state": self.game_state.to_dict()}
             )
         )
 
     async def disconnect(self, close_code):
-        # Abandonar el grupo de canales
-        if not hasattr(self, "game_group_name"):
-            return
-        await self.channel_layer.group_discard(self.game_group_name, self.channel_name)
+        self.update_game_state_task.cancel()
 
     async def receive(self, text_data=None, bytes_data=None):
         if text_data:
@@ -180,43 +160,59 @@ class GameConsumer(AsyncWebsocketConsumer):
         if not event.get("keys"):
             return
 
-        keys = event["keys"]
+        async with self.redis.pipeline() as pipe:
+            while True:
+                try:
+                    # Make sure we are the only ones updating the game state
+                    await pipe.watch(f"game:{self.game.id}")
 
-        for key in keys:
-            direction_multiplier = 1 if key in ["arrowDown", "s"] else -1
+                    await self.load_game_state(pipe)
+                    
+                    for key in event["keys"]:
+                        direction_multiplier = 1 if key in ["arrowDown", "s"] else -1
 
-            for controller in self.left_paddle_controller:
-                if (controller == "letters" and key in ["w", "s"]) or (
-                    controller == "arrows" and key in ["arrowUp", "arrowDown"]
-                ):
-                    self.game_state.left.paddle_y += (
-                        self.paddle_move_amount * direction_multiplier
+                        for controller in self.left_paddle_controller:
+                            if (controller == "letters" and key in ["w", "s"]) or (
+                                controller == "arrows" and key in ["arrowUp", "arrowDown"]
+                            ):
+                                self.game_state.left.paddle_y += (
+                                    self.paddle_move_amount * direction_multiplier
+                                )
+
+                        for controller in self.right_paddle_controller:
+                            if (controller == "letters" and key in ["w", "s"]) or (
+                                controller == "arrows" and key in ["arrowUp", "arrowDown"]
+                            ):
+                                self.game_state.right.paddle_y += (
+                                    self.paddle_move_amount * direction_multiplier
+                                )
+
+                    # Limit paddle movement
+                    self.game_state.left.paddle_y = max(
+                        self.paddle_height / 2,
+                        min(
+                            self.field_height - self.paddle_height / 2,
+                            self.game_state.left.paddle_y,
+                        ),
                     )
 
-            for controller in self.right_paddle_controller:
-                if (controller == "letters" and key in ["w", "s"]) or (
-                    controller == "arrows" and key in ["arrowUp", "arrowDown"]
-                ):
-                    self.game_state.right.paddle_y += (
-                        self.paddle_move_amount * direction_multiplier
+                    self.game_state.right.paddle_y = max(
+                        self.paddle_height / 2,
+                        min(
+                            self.field_height - self.paddle_height / 2,
+                            self.game_state.right.paddle_y,
+                        ),
                     )
 
-        # Limit paddle movement
-        self.game_state.left.paddle_y = max(
-            self.paddle_height / 2,
-            min(
-                self.field_height - self.paddle_height / 2,
-                self.game_state.left.paddle_y,
-            ),
-        )
-
-        self.game_state.right.paddle_y = max(
-            self.paddle_height / 2,
-            min(
-                self.field_height - self.paddle_height / 2,
-                self.game_state.right.paddle_y,
-            ),
-        )
+                    # now we can put the pipeline back into buffered mode with MULTI
+                    pipe.multi()
+                    await self.save_game_state(pipe)
+                    await pipe.execute()
+                    break
+                except redis.WatchError:
+                    print("WatchError, retrying")
+                    # Retry if someone else modified the game state
+                    continue
 
     def check_collisions(self):
         # Top and bottom walls
@@ -246,16 +242,16 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.game_state.right.score += 1
             self.game_state.ball.x = self.field_width / 2
             self.game_state.ball.y = self.field_height / 2
-            self.game_state.ball.dx = self.initial_ball_speed # Serve to the right
+            self.game_state.ball.dx = self.initial_ball_speed  # Serve to the right
             self.game_state.ball.dy = random.choice([-1, 1]) * self.initial_ball_speed
             return
-        
+
         # Right wall
         if self.game_state.ball.x >= self.field_width:
             self.game_state.left.score += 1
             self.game_state.ball.x = self.field_width / 2
             self.game_state.ball.y = self.field_height / 2
-            self.game_state.ball.dx = -self.initial_ball_speed # Serve to the left
+            self.game_state.ball.dx = -self.initial_ball_speed  # Serve to the left
             self.game_state.ball.dy = random.choice([-1, 1]) * self.initial_ball_speed
             return
 
@@ -287,17 +283,11 @@ class GameConsumer(AsyncWebsocketConsumer):
             self.game_state.ball.dx *= -self.ball_speed_increment
             self.game_state.ball.dy *= self.ball_speed_increment
 
-    async def game_loop(self):
-
+    async def update_game_state(self):
         while True:
-            # Comprobar colisiones
-            self.check_collisions()
+            await self.load_game_state(self.redis)
 
-            # Mover la bola
-            self.game_state.ball.x += self.game_state.ball.dx
-            self.game_state.ball.y += self.game_state.ball.dy
-
-            await self.save_and_broadcast_game_state()
+            await self.send_game_state()
 
             # Esperar hasta el siguiente frame
             await asyncio.sleep(1 / self.fps)
