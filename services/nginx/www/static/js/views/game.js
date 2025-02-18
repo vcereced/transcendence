7d7aac8a2e6d1a333387a8009e014ef681c969a2
fer5899 <fdometallic@gmail.com>
2025-02-18 12:10:20 +0100

    Extract auth_management

--- services/nginx/www/static/js/views/game.js
+++ services/nginx/www/static/js/views/game.js
@@ -1,4 +1,5 @@
 import { handleJwtToken } from './jwtValidator.js';
+import { getCookie, deleteCookie, hasAccessToken } from '../utils/auth_management.js';
 //static/js/views/game.js
 
 export function renderGame() {
@@ -227,41 +228,4 @@ export async function initGame() {
         setTimeout(gameLoop, 1000 / fps); // Approximately 60 frames per second
     }
 
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
-function deleteCookie(cname) {
-    document.cookie = cname + '=;';
-}
-
-function decodeJWT(token) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-
-    return JSON.parse(jsonPayload);
-}
-
-function hasAccessToken() {
-    if (getCookie("accessToken") === "") {
-        return false;
-    }
-    return true;
 }
