e23ec9474ecd969709d6c93e6f815bf0e011d84c
fer5899 <fdometallic@gmail.com>
2025-04-01 16:02:51 +0200

    Add match history url

--- services/game/game_app/urls.py
+++ services/game/game_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("active/", views.has_active_game),
     path("create/", views.create_game),
 	path("statistics/<int:user_id>/", views.get_match_statistics),
+	path("history/<int:user_id>/", views.get_match_history),
 ]
