47355cfa3ea5fe65ccf2f9bb0506011e7a0cd71f
David Garizado <dgarizad@c2r5s1.42malaga.com>
2025-04-10 17:43:38 +0200

    updates to Matchmaking

--- services/auth/auth_project/settings.py
+++ services/auth/auth_project/settings.py
@@ -101,7 +101,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),  # Duración del token
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50),  # Duración del token
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),     # Duración del token de refresco
     'ROTATE_REFRESH_TOKENS': True,                  # Renueva el refresh token al utilizarlo
     'BLACKLIST_AFTER_ROTATION': True,               # Evitar que se reutilicen tokens de refresco rotados
