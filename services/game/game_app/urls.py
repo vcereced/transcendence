bcdd0b52869e0fdb799e8d9c8f9cc0c59cc64f07
fer5899 <fdometallic@gmail.com>
2025-04-22 19:15:20 +0200

    Fix url error in game

--- services/game/game_app/urls.py
+++ services/game/game_app/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from game_app import views
 
 urlpatterns = [
-    path("trigger_create_game/", views.trigger_create_game_task),
-    path("trigger_launch_game/", views.trigger_launch_game_task),
     path("active/", views.has_active_game),
     path("create/", views.create_game),
 	path("statistics/<int:user_id>/", views.get_match_statistics),
